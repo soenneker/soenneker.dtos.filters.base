7b9c8bd61568f67abd34c0db106e2d709453243c
Jake Soenneker <jake@soenneker.com>
2025-08-14 19:41:14 -0500

    PublicOpenApiObject

--- src/FilterBase.cs
+++ src/FilterBase.cs
@@ -1,11 +1,13 @@
 ﻿using System.Text.Json.Serialization;
 using Newtonsoft.Json;
+using Soenneker.Attributes.PublicOpenApiObject;
 
 namespace Soenneker.Dtos.Filters.Base;
 
 /// <summary>
 /// An abstract generic object for handling filtering during querying
 /// </summary>
+[PublicOpenApiObject]
 public abstract class FilterBase
 {
     [JsonPropertyName("field")]
